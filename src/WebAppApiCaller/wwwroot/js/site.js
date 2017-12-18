bba8ddb6b7c4598126f2b5e9e38a0e9aeae43428
RussW <russ.h.walker@gmail.com>
2017-12-17 21:57:16 -0500

    seeting up function and caller app

---
+++ src/WebAppApiCaller/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Write your JavaScript code.
+
+var myHeaders = new Headers();
+
+var init = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default'
+};
+
+fetch('', init)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    }).then(function (json) {
+        alert(JSON.stringify(json));
+    }).catch(function (error) {
+        console.log('There has been a problem with your fetch operation: ', error.message);
+    });
